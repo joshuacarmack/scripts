f2f2cc516c9c68a62219cefdb8e22c6fda83f33d
Joshua Carmack <joshuahcarmack@gmail.com>
2024-06-18 12:59:30 -0400

    remove wazuh

--- powershell/InstallWazuhAgent.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.7.3-1.msi -OutFile ${env.tmp}\wazuh-agent; msiexec.exe /i ${env.tmp}\wazuh-agent /q WAZUH_MANAGER='jclab.xyz' WAZUH_REGISTRATION_SERVER='jclab.xyz' 
-Get-Service; Start-Sleep -Seconds 85; Get-Service
-net start wazuhsvc
